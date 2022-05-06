4415e572e48d3d8258b3a6fcc347387d0ff7e093
Wanderson Marques <wanderson.marques@pixter.com.br>
2022-05-06 00:27:41 -0300

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express")
 const http = require("http")
 const app = express()
 const server = http.createServer(app)
-
 const io = require("socket.io")(server, {
 	cors: {
 		origin: "http://localhost:3000",
@@ -26,4 +25,4 @@ io.on("connection", (socket) => {
 	})
 })
 
-module.exports = io;
+server.listen(5000, () => console.log("server is running on port 5000"))
